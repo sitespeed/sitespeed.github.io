20e74ece16d28feeafc9dc052708052266db6b01
sitespeed <hp.singh@live.in>
2016-04-14 01:55:53 +0530

    Update

---
+++ assets/js/pop-up.js
@@ -0,0 +1,4 @@
+function newPopup(url) {
+	popupWindow = window.open(
+		url,'popUpWindow','height=600,width=500,left=10,top=10,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no,status=yes')
+}
